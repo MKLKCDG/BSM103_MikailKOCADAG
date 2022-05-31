2874890f72df0a097d96a158ece282050c942504
Mikail KOCADAĞ <20010310036@ogrenci.bartin.edu.tr>
2022-05-31 15:34:11 +0300

    week14

---
+++ 2021-2022 Bahar Dönemi/Week14/InsertionSort.java
@@ -0,0 +1,25 @@
+class InsertionSort{
+	void insertinSort(int[] array){
+		for(int i=1;i<array.length;i++){
+			int key =array[i];
+			int j=i-1;
+			while(j>=0&&array[j]>key){
+				array[j+1]=array[j];
+				j--;
+			}
+			array[j+1]=key;
+		}
+	}
+	void printArray(int[] array){
+		for(int i=0;i<array.length;i++){
+			System.out.print(array[i]+" ");
+		}
+		System.out.println();
+	}
+	public static void main(String[] args){
+		int[] array ={9,7,6,15,17,5,10,11};
+		InsertionSort ob = new InsertionSort();
+		ob.insertinSort(array);
+		ob.printArray(array);
+	}
+}
---
+++ 2021-2022 Bahar Dönemi/Week14/MegaSort.java
@@ -0,0 +1,60 @@
+public class MegaSort{
+	void sort(int[] array,int left,int right){
+		if(left<right){
+			int middle = left+(right-left)/2;
+			sort(array,left,middle);
+			sort(array,middle+1,right);
+			merge(array,left,middle,right);
+		}
+	}
+	void merge(int[] array,int left,int middle,int right){
+		int number1=middle-left+1;
+		int number2=right-middle;
+		int[] L=new int [number1];
+		int[] R =new int[number2];
+		
+		for(inti=0;i<number1;i++){
+			L[i]=array[middle+1+i];
+		}
+		for(inti=0;i<number2;i++){
+			L[i]=array[middle+1+i];
+		}
+		
+		int i=0,j=0;
+		int k=left;
+		while(i<number1&&j<number2){
+			if(L[i]<=R[j]){
+				array[k]=L[i];
+				i++;
+			}else{
+				array[k]=R[j];
+				j++;
+			
+			k++;
+		}
+		while(i<number1){
+			array[k]=L[i];
+			i++;
+			k++;
+		}
+		while(i<number2){
+			array[k]=L[i];
+			i++;
+			k++;
+		}
+	}
+	
+	void printArray(int[] array){
+		for(int i=0;i<array.length;i++){
+			System.out.print(array[i]+" ");
+		}
+		System.out.println();
+	}
+	public static void main(String[]args){
+		
+		MegaSort ob = new MegaSort();
+		int[] array ={38,27,43,3,9,82,10};
+		ob.sort(array,0,array.length-1);
+		ob.printArray(array);
+	}
+}
